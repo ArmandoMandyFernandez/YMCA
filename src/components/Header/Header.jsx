1e905a9528605efa10ef2941dacf4851e3661413
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-05-09 13:07:11 -0400

    starting the header and added a background color

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,17 @@
 import './Header.scss';
-import logo from '../../assets/images/logo-no-background.svg'
+import logo from '../../assets/images/logo-no-background.png';
 
 function Header(){
     return(
     <header className='navBar'>
         <div className='navBar__content'>
-            <div className='navBar__logo-container'>
-                <img src={logo} alt="" />
+            <div className='navBar__content-container'>
+                <img src={logo} alt="ymca logo" className='navBar__logo'/>
             </div>
-
+            <div>
+                <h4>Armando Fernandez</h4>
+            </div>
+            <div></div>
         </div>
     </header>
 
