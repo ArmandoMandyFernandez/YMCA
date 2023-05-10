3f825ef47bbd66bcc5619f4b2a70fb11ae57f76c
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-05-10 11:11:16 -0400

    added emojis to the subsections of the budget overview

--- src/components/BudgetOverview/BudgetOverview.jsx
+++ src/components/BudgetOverview/BudgetOverview.jsx
@@ -2,28 +2,34 @@ import "./BudgetOverview.scss";
 import ProgressBar from 'react-bootstrap/ProgressBar';
 
 function BudgetOverview() {
+    const nowHome = 90;
+    const nowUtils = 85;
+    const nowTrans = 75;
+    const nowEnt = 37;
+    const nowFood = 82;
+
     return (
         <section className="budgetOverview">
             <h2>Budget</h2>
             <div className="budgetOverview__category-container">
-                <h5>Home</h5>
-                <ProgressBar animated now={60} />
+                <h5>Home 🏠</h5>
+                <ProgressBar animated now={90} label={`${nowHome}%`}/>
             </div>
             <div className="budgetOverview__category-container">
-                <h5>Utilities</h5>
-                <ProgressBar animated now={85} />
+                <h5>Utilities 🚰</h5>
+                <ProgressBar animated now={85} label={`${nowUtils}%`}/>
             </div>
             <div className="budgetOverview__category-container">
-                <h5>Transportation</h5>
-                <ProgressBar animated now={40} />
+                <h5>Transportation 🚙</h5>
+                <ProgressBar animated now={75} label={`${nowTrans}%`}/>
             </div>
             <div className="budgetOverview__category-container">
-                <h5>Entertainment</h5>
-                <ProgressBar animated now={37} />
+                <h5>Entertainment 🍿</h5>
+                <ProgressBar animated now={37} label={`${nowEnt}%`}/>
             </div>
             <div className="budgetOverview__category-container">
-                <h5>Food</h5>
-                <ProgressBar animated now={82} />
+                <h5>Food 🍔</h5>
+                <ProgressBar animated now={82} label={`${nowFood}%`}/>
             </div>
         </section>
     );
