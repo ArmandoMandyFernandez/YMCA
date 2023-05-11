bab65caf227df1be2864f4a48321f777d8a13d3b
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-05-11 13:41:43 -0400

    added routes for buttons

--- src/Pages/QuestionsPage/QuestionsPage.jsx
+++ src/Pages/QuestionsPage/QuestionsPage.jsx
@@ -1,5 +1,5 @@
 import "./QuestionsPage.scss";
-import { Combobox } from "@headlessui/react";
+import { Link } from "react-router-dom";
 
 function QuestionsPage() {
     return (
@@ -11,34 +11,67 @@ function QuestionsPage() {
             <form action="submit" className="questions__form">
                 <label htmlFor="" className="questions__form-label">
                     <h5>What is your Monthly Net Income?</h5>
-                    <input type="number" name="" id="" className="questions__form-input"/>
+                    <input
+                        type="number"
+                        name=""
+                        id=""
+                        className="questions__form-input"
+                    />
                     <p>Net income definition.</p>
                 </label>
                 <label htmlFor="" className="questions__form-label">
                     <h5>Rent/Mortgage</h5>
-                    <input type="number" name="" id="" className="questions__form-input"/>
+                    <input
+                        type="number"
+                        name=""
+                        id=""
+                        className="questions__form-input"
+                    />
                     <p>Rent/Mortgage definition</p>
                 </label>
                 <label htmlFor="" className="questions__form-label">
                     <h5>Utilities</h5>
-                    <input type="number" name="" id="" className="questions__form-input"/>
+                    <input
+                        type="number"
+                        name=""
+                        id=""
+                        className="questions__form-input"
+                    />
                     <p>Utilities definition</p>
                 </label>
                 <label htmlFor="" className="questions__form-label">
                     <h5>Transportation</h5>
-                    <input type="number" name="" id="" className="questions__form-input"/>
+                    <input
+                        type="number"
+                        name=""
+                        id=""
+                        className="questions__form-input"
+                    />
                     <p>How much are you spending on cars</p>
                 </label>
                 <label htmlFor="" className="questions__form-label">
                     <h5>Entertainment</h5>
-                    <input type="number" name="" id="" className="questions__form-input"/>
+                    <input
+                        type="number"
+                        name=""
+                        id=""
+                        className="questions__form-input"
+                    />
                     <p>Going out too much</p>
                 </label>
                 <label htmlFor="" className="questions__form-label">
                     <h5>Food</h5>
-                    <input type="number" name="" id="" className="questions__form-input"/>
+                    <input
+                        type="number"
+                        name=""
+                        id=""
+                        className="questions__form-input"
+                    />
                     <p>STOP EATING OUT YOU FATTY</p>
                 </label>
+                <Link to="/profile">
+                    <button className="questions__form-button">Submit</button>
+                </Link>
             </form>
         </section>
     );
--- src/components/BeginButton/BeginButton.jsx
+++ src/components/BeginButton/BeginButton.jsx
@@ -1,8 +1,13 @@
 import './BeginButton.scss';
+import { Link } from 'react-router-dom';
+
 
 function BeginButton(){
+
     return(
+        <Link to='questions'>
         <button className='beginButton'>Begin</button>
+        </Link>
     );
 }
 export default BeginButton;
