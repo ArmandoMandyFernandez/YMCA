14c814b7844cd35812f9a8685b1b4738b5407535
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-05-17 14:48:56 -0400

    uncommitted changes from last push

--- src/Pages/DashboardPage/DashboardPage.jsx
+++ src/Pages/DashboardPage/DashboardPage.jsx
@@ -9,7 +9,6 @@ function DashboardPage() {
     useEffect(() => {
         const observer = new IntersectionObserver((entries) => {
             entries.forEach((entry) => {
-                console.log(entry);
                 if (entry.isIntersecting) {
                     entry.target.classList.add("zhow");
                 } else {
